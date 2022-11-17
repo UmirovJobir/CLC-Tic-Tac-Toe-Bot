e4f7bc598cec552e056ce7b5c9afeba437a82fd7
Flurt <umidrukhiddinov70@gmail.com>
2022-11-17 17:36:24 +0500

    test

--- main.py
+++ main.py
@@ -18,22 +18,22 @@ dispatcher: Dispatcher = updater.dispatcher
 class Game:
     def __init__(self, context: CallbackContext) -> None:
         self._context = context
-        self._chat_data = context.chat
-        self._chat_data.update({
+        self._bot_data = context.bot_data
+        self._bot_data.update({
             'game_increment': 1,
         })
         self.game_name = None
         self.game = None
 
     def _get_next_game_id(self):
-        _id = self._chat_data['game_increment']
-        self._chat_data.update({
+        _id = self._bot_data['game_increment']
+        self._bot_data.update({
             'game_increment': _id + 1
         })
         return _id
 
     def store_data(self):
-        self._chat_data.update({
+        self._bot_data.update({
             self.game_name: self.game
         })
 
@@ -48,13 +48,38 @@ class Game:
         self.store_data()
 
     def get_game(self, name):
-        self.game = self._chat_data.get(name, None)
+        self.game = self._bot_data.get(name, None)
+
+
+    def make_move(self, user: User, index: int):
+        game = self.game
+        player1 = game['player1']
+        player2 = game['player2']
+        is_player1_first = self.game['is_player1_first']
+
+        if player2['id'] is None:
+            if user.id != player1['id']:
+                self.set_player2(user)
+                player2 = game['player2']
+            else:
+                return
+
+        if not (player1['id'] == user.id or
+                player2['id'] == user.id):
+            return
+
+        turn = game['turn']
+
+        if index is None:
+            return
+
+        current_player = player2 if turn else player
 
 
 def generate_keyboard(game: Optional[Game]):
     NONE = "⏹"
     CROSS = "❌"
-    CIRCLE = "⭕️"
+    CIRCLE = "⭕"
 
     keyboard = []
 
@@ -64,9 +89,9 @@ def generate_keyboard(game: Optional[Game]):
             index = i * 3 + j
             if game is None:
                 item = 0
-                callback_data = f'new_game|{index}'
+                callback_data = f'new_game'
             else:
-                item = game[index]
+                item = game.game['game'][index]
                 callback_data = f'{game.game_name}|{index}'
             # item = game[index]
             text = NONE if item == 0 else CROSS if item == 1 else CIRCLE
@@ -99,16 +124,38 @@ def callback_query(update: Update, context: CallbackContext):
 
     query.answer()
 
+    game = Game(context)
+    is_player1_first = True
+    index = None
+
+    if 'new_game' in query.data:
+        is_player1_first = \
+            True if query.data.split('|')[1] == 'True' else False
+        game.new_game(is_player1_first=is_player1_first)
+        game.set_player1(update.effective_user)
+    else:
+        game_name, index = query.data.split('|')
+        index = int(index)
+        game.get_game(game_name)
+        if game.game is None:
+            game.new_game(is_player1_first=is_player1_first)
+            game.set_player1(update.effective_user)
+
+    game.make_move(update.effective_user, index)
+
+    print(game.game)
+
+    query.edit_message_text(
+        text=f"selected option",
+        reply_markup=InlineKeyboardMarkup(generate_keyboard(game)))
+
     print(query)
 
     # index = int(query.data)
-
-    # game = [0,0,0,0,0,0,0,0,0] 
+    #
+    # game = [0,0,0,0,0,0,0,0,0]
     # game[index] = 1
-
-    # query.edit_message_text(
-    #     text=f"selected option", 
-    #     reply_markup=InlineKeyboardMarkup(generate_keyboard(game)))
+    #
 
 
 dispatcher.add_handler(InlineQueryHandler(inline_query))
