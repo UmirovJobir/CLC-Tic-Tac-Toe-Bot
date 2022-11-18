dddfda3f920f27221d8d50e720e5f6ad5e41189d
Flurt <umidrukhiddinov70@gmail.com>
2022-11-18 16:20:40 +0500

    next

--- main.py
+++ main.py
@@ -2,18 +2,29 @@ from typing import Optional
 
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultArticle, InputTextMessageContent
 from telegram.ext import Updater, Dispatcher, CommandHandler, CallbackContext, MessageHandler, Filters, \
-    InlineQueryHandler, CallbackQueryHandler
+    InlineQueryHandler, CallbackQueryHandler, DictPersistence
 from telegram.update import Update
 import logging
+import settings
 
 logging.basicConfig(
     format='%(asctime)s-%(name)s-%(levelname)s-%(message)s',
     level=logging.DEBUG)
-
-updater = Updater(token='5654862420:AAEK0j4WzOWXLwFNGoPGH1WP_yLS4RK1gHs')
+persistence = DictPersistence()
+updater = Updater(
+    token=settings.TOKEN,
+    persistence=persistence
+    )
 
 dispatcher: Dispatcher = updater.dispatcher
 
+NONE = '⬜'
+CROSS = '❌'
+CIRCLE = '⭕'
+HAND = '👈'
+WINNER = '🤩'
+LOSER = '😭'
+DRAW = '😡'
 
 class Game:
     def __init__(self, context: CallbackContext) -> None:
@@ -33,23 +44,93 @@ class Game:
         return _id
 
     def store_data(self):
+        temp = self.game.copy()
+        temp.update({
+            'grid': self.game['grid'].items
+        })
         self._bot_data.update({
-            self.game_name: self.game
+            self.game_name: temp
         })
 
     def new_game(self):
         self.game_name = 'game' + str(self._get_next_game_id())
         self.game_name = {
-            'player1': None,
-            'player2': None,
-            'game': [0, 0, 0, 0, 0, 0, 0, 0, 0],
-            'turn': False,  # False <- player1 / True <- player2
+            'player1': {
+                'id': None,
+                'name': '?'
+            },
+            'player2': {
+                'id': None,
+                'name': '?'
+            },
+            'grid': Grid([]),
+            # False <- player1
+            # True <- player2
+            'turn': False,
+            # True <- player1 = x, player2 = o
+            # False <- player1 = o, player2 = x
+            'is_player1_first': is_player1_first,
         }
         self.store_data()
 
-    def get_game(self, name):
-        self.game = self._bot_data.get(name, None)
+    def get_turn_message(self):
+        game = self.game
+        player1 = game['player1']
+        player2 = game['player2']
+        turn = game['turn']
+        is_player_first = game['is_player1_first']
 
+        return f"{CROSS if is_player_first else CIRCLE}" \
+               f"{player1['name']}" \
+               f"{'' if turn else HAND}" \
+               f"\n" \
+               f"{CIRCLE if is_player_first else CROSS}" \
+               f"{player2['name']}" \
+               f"{HAND if turn else ''}"
+
+
+    def get_game(self, name):
+        self.game_name = name
+        self.game = self._bot_data.get(name, None)
+        if self.game is not None:
+            temp = self.game.get('grid')
+            grid = Grid([])
+            if temp is list:
+                grid = Grid(temp)
+            self.game.update({
+                'grid': grid
+            })
+
+    def get_end_message(self):
+        game = self.game
+        player1 = game['player1']
+        player2 = game['player2']
+        is_player_first = game['is_player1_first']
+        grid: Grid = self.game['grid']
+        winner = grid.get_winner()
+        player1_emoji = DRAW
+        player2_emoji = DRAW
+
+        if winner:
+            if is_player_first:
+                player1_emoji = WINNER if winner == 1 else LOSER
+                player2_emoji = WINNER if winner == 2 else LOSER
+            else:
+                player1_emoji = LOSER if winner == 1 else WINNER
+                player2_emoji = LOSER if winner == 2 else WINNER
+
+        return f"{CROSS if is_player_first else CIRCLE}" \
+               f" {player1['name']} " \
+               f"{player1_emoji}" \
+               f"\n" \
+               f"{CIRCLE if is_player_first else CROSS}" \
+               f" {player2['name']} " \
+               f"{player2_emoji}"
+
+    def switch_turn(self):
+        turn = self.game['turn']
+        self.game['turn'] = not turn
+        self.store_data()
 
     def make_move(self, user: User, index: int):
         game = self.game
@@ -73,7 +154,23 @@ class Game:
         if index is None:
             return
 
-        current_player = player2 if turn else player
+        current_player = player2 if turn else player1
+
+        if current_player['id'] != user.id:
+            return
+
+        grid = game['grid']
+
+        can_turn = grid.items[index] == 0
+
+        if not can_turn:
+            return
+
+        grid.items[index] = 1 if is_player1_first and not turn else 2
+
+        self.store_data()
+
+        self.switch_turn()
 
 
 def generate_keyboard(game: Optional[Game]):
